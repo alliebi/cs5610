c7fb1564a009ff38c5f3ce5e58bc37ee5c97b810
abi <bi.a@husky.neu.edu>
2019-03-29 20:09:01 -0700

    Add mLab URI

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,12 +1,12 @@
-// mongodb://<dbuser>:<dbpassword>@ds113505.mlab.com:13505/webdev
-var connectionString = 'mongodb://127.0.0.1:27017/webdev';
-//var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
-var mongoose = require("mongoose");
+// var connectionString = 'mongodb://127.0.0.1:27017/webdev';
+const connectionString = 'mongodb://admin:abc123456@ds017165.mlab.com:17165/heroku_xhj2gz7c';
+
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const client = mongoose.connect( connectionString, { useNewUrlParser: true });
 
 //Get the default connection
-var db = mongoose.connection;
+const db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
